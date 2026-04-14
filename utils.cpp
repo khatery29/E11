6f85afef7cb6c5a8f0ec7847a97d9d6d8903410c
Yousef Khater <khatery@aucegypt.edu>
2026-04-14 14:14:23 +0200

    utils

---
+++ utils.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <regex>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+void check_email (string email) {
+
+	regex pattern(R"(.+@.+\..+)");
+
+	if (!(regex_match(email, pattern))) {
+		throw invalid_argument("This is an invalid email");
+	}
+}
